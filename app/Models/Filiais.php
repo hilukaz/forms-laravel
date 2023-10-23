7e88900143deab643922f128fb2b27deec0d72e0
hilukaz <lucaskawakami5@gmail.com>
2023-10-22 21:02:52 -0300

    gitlixo

--- app/Models/Filiais.php
+++ app/Models/Filiais.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class FILIAIS extends Model
+class Filiais extends Model
 {
     use HasFactory;
 
